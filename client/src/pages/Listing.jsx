8d238cf179dd5992922adaf6115842a7de8651a7
celetrialPrince166 <princeayiku5@gmai.com>
2024-07-19 12:20:04 +0000

    contact button remove

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -139,7 +139,7 @@ export default function Listing() {
                 {listing.instagram}
               </li>
             </ul>
-            {currentUser && listing.userRef !== currentUser._id && !contact && (
+            {/* {currentUser && listing.userRef !== currentUser._id && !contact && (
               <button
                 onClick={() => setContact(true)}
                 className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
@@ -147,7 +147,7 @@ export default function Listing() {
                 Contact
               </button>
             )}
-            {contact && <Contact listing={listing} />}
+            {contact && <Contact listing={listing} />} */}
           </div>
         </div>
       )}
